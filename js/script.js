a7c25383699a3f498056d83460c065b63ebe92bc
meikalaheshla <fausto.boshoff@gmail.com>
2022-07-11 15:38:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+console.log(Vue)
+
+const root = new Vue({
+    name: 'titolo',
+    el: '#root',
+    data: {
+        title: 'TESTO GRANDE',
+        img: 'https://picsum.photos/200/300',
+        alt: 'immagine random'
+    }
+})
